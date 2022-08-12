d80af9f628c0e4d3c92539aada62ac7214449dbc
MusicCookie <111118871+MusicCookie@users.noreply.github.com>
2022-08-12 11:31:28 +0200

    Update dl.py

--- dl.py
+++ dl.py
@@ -1,12 +1,11 @@
 import sys, os
 from skillshare import Skillshare, splash
-from magic import cookie
 
 # or by class ID:
 # dl.download_course_by_class_id(189505397)
 
 def main():
-    dl = Skillshare(cookie)
+    dl = Skillshare(f67fccdb6f39ad13465ac2e90b971dfe)
     course_url = sys.argv[1]
     dl.download_course_by_url(course_url)
 
